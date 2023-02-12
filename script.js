e7c1201714b2719504ea39dd17f5cf952533e7ba
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-02-12 21:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function() {
+  const ropeLengthsInput = document.getElementById("ropeLengths").value;
+  const ropeLengths = ropeLengthsInput.split(",").map(function(length) {
+    return parseInt(length, 10);
+  });
+  const result = minimumCost(ropeLengths);
+  document.getElementById("result").innerHTML = "Minimum cost: " + result;
+});
+
+function minimumCost(arr) {
+  arr.sort((a, b) => a - b);
+  let cost = 0;
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift();
+    let currentCost = first + second;
+    cost += currentCost;
+    arr.unshift(currentCost);
+    arr.sort((a, b) => a - b);
+  }
+  return cost;
+}
